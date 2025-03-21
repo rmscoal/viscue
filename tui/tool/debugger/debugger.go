05833ecf252c2bb9d9b5a96d50f7aaf3009014d4
Rifky Manuel Satyana <101387958+rmscoal@users.noreply.github.com>
2025-03-21 22:37:43 +0700

    fix: path to a logger file using hidden and located at homedir (#5)

---
+++ tui/tool/debugger/debugger.go
@@ -0,0 +1,33 @@
+package debugger
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+)
+
+func New() (file *os.File, err error) {
+	homedir := "./"
+	filename := "debug.log"
+	log.SetLevel(log.DebugLevel)
+
+	_, ok := os.LookupEnv("DEBUG")
+	if !ok {
+		filename = ".viscue.error.log"
+		log.SetLevel(log.ErrorLevel)
+		homedir, err = os.UserHomeDir()
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch home directory")
+		}
+	}
+
+	path := filepath.Join(homedir, filename)
+	file, err = tea.LogToFileWith(path, "", log.Default())
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
